585133eeb822a4a8f48314fe493ded8a75990859
Deekshith Bommisetty <writingtodeek@gmail.com>
2021-01-23 16:54:12 -0500

    mongoose model and validation

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 // Connect Database
 connectDB();
 
+// Init Middlewar
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
